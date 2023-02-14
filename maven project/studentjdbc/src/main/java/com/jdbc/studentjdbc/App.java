292633c3c7639bb202d44f7363209721a9c6ad55
Yash <121216509+yashdhanawde@users.noreply.github.com>
2023-02-14 19:51:49 +0530

    Add files via upload

---
+++ maven project/studentjdbc/src/main/java/com/jdbc/studentjdbc/App.java
@@ -0,0 +1,13 @@
+package com.jdbc.studentjdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static voidmain( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
